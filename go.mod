1645876fc6f4284fcd00075a710cf23f47c090c9
lomtom <lstack@xlauncher.io>
2022-07-04 08:05:11 +0800

    最小绝对差

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module algorithm
 
-go 1.16
+go 1.18
+
+require github.com/lomtom/go-utils/v2 v2.0.0-20220617030149-b6fd83f48eb6
---
+++ leetcode/1200最小绝对差_test.go
@@ -0,0 +1,46 @@
+package leetcode
+
+import (
+	"github.com/lomtom/go-utils/v2/assert"
+	"sort"
+	"testing"
+)
+
+func minimumAbsDifference(arr []int) [][]int {
+	sort.Ints(arr)
+	var min int = int(^uint(0) >> 1)
+	for i := 1; i < len(arr); i++ {
+		temp := arr[i] - arr[i-1]
+		if temp < min {
+			min = temp
+		}
+	}
+	res := make([][]int, 0)
+	for i := 1; i < len(arr); i++ {
+		if arr[i]-arr[i-1] == min {
+			res = append(res, []int{arr[i-1], arr[i]})
+		}
+	}
+	return res
+}
+
+func Test(t *testing.T) {
+	collections := []struct {
+		input  []int
+		output [][]int
+	}{
+		{
+			input:  []int{4, 2, 1, 3},
+			output: [][]int{{1, 2}, {2, 3}, {3, 4}},
+		},
+		{
+			[]int{1, 3, 6, 10, 15},
+			[][]int{{1, 3}},
+		},
+	}
+
+	s := assert.NewAssert(t)
+	for index := range collections {
+		s.Equal(minimumAbsDifference(collections[index].input), collections[index].output)
+	}
+}
