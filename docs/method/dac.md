dd7823442cc1894762e4ba48575810a1ba09f887
lomtom <lomtom@qq.com>
2022-07-21 08:19:28 +0800

    814二叉树剪枝

--- docs/method/dac.md
+++ docs/method/dac.md
@@ -24,3 +24,4 @@
 中等
 - [241、为运算表达式设计优先级](https://leetcode.cn/problems/different-ways-to-add-parentheses/)
 - [558、四叉树交集](../leetcode/558四叉树交集_readme.md)
+- [814、二叉树剪枝](../leetcode/814二叉树剪枝_readme.md)
---
+++ leetcode/814二叉树剪枝_test.go
@@ -0,0 +1,22 @@
+package leetcode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func pruneTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left = pruneTree(root.Left)
+	root.Right = pruneTree(root.Right)
+	if root.Left == nil && root.Right == nil && root.Val == 0 {
+		return nil
+	}
+	return root
+}
