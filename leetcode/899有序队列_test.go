ee241814a185e25f32845662ee9ef9fadc0d4d7a
lomtom <lomtom@qq.com>
2022-08-03 08:46:38 +0800

    899. 有序队列

---
+++ leetcode/899有序队列_test.go
@@ -0,0 +1,19 @@
+package leetcode
+
+import "sort"
+
+func orderlyQueue(s string, k int) string {
+	if k == 1 {
+		ans := s
+		for i := 1; i < len(s); i++ {
+			s = s[1:] + s[:1]
+			if s < ans {
+				ans = s
+			}
+		}
+		return ans
+	}
+	t := []byte(s)
+	sort.Slice(t, func(i, j int) bool { return t[i] < t[j] })
+	return string(t)
+}
