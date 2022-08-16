f4078b1327efcf63fc133d625ed2672e3d9ac474
lomtom <lomtom@qq.com>
2022-08-16 16:46:52 +0800

    1656设计有序流

---
+++ leetcode/1656/1656设计有序流_test.go
@@ -0,0 +1,31 @@
+package _656
+
+//执行耗时:60 ms,击败了100.00% 的Go用户
+//内存消耗:7.2 MB,击败了15.79% 的Go用户
+type OrderedStream struct {
+	nums  []string
+	index int
+}
+
+func Constructor(n int) OrderedStream {
+	return OrderedStream{
+		make([]string, n),
+		0,
+	}
+}
+
+func (this *OrderedStream) Insert(idKey int, value string) (ans []string) {
+	this.nums[idKey-1] = value
+	if this.index != idKey-1 {
+		return
+	}
+	for this.index < len(this.nums) {
+		if this.nums[this.index] == "" {
+			break
+		} else {
+			ans = append(ans, this.nums[this.index])
+		}
+		this.index++
+	}
+	return
+}
