32319db714dc4adf9fddb708b442db81337d459c
lomtom <lomtom@qq.com>
2022-08-29 08:29:32 +0800

    1470重新排列数组
    Signed-off-by: lomtom <lomtom@qq.com>

--- leetcode/1470重新排列数组_test.go
+++ leetcode/1470重新排列数组_test.go
@@ -1,5 +1,7 @@
 package leetcode
 
+//执行用时：4 ms, 在所有 Go 提交中击败了86.57%的用户
+//内存消耗：3.7 MB, 在所有 Go 提交中击败了19.40%的用户
 func shuffle(nums []int, n int) (ans []int) {
 	for i := 0; i < n; i++ {
 		ans = append(ans, nums[i], nums[i+n])
