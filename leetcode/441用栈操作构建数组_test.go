c66d227d916c4bc8973b492db52e69bc23510ea2
lomtom <lomtom@qq.com>
2022-10-15 22:06:57 +0800

    441用栈操作构建数组
    Signed-off-by: lomtom <lomtom@qq.com>

--- leetcode/441用栈操作构建数组_test.go
+++ leetcode/441用栈操作构建数组_test.go
@@ -27,6 +27,21 @@ func buildArray(target []int, n int) (ans []string) {
 	return
 }
 
+//执行耗时:0 ms,击败了100.00% 的Go用户
+//内存消耗:2.2 MB,击败了85.00% 的Go用户
+func buildArray1(target []int, n int) (ans []string) {
+	var index int = 0
+	for i := 1; i <= target[len(target)-1]; i++ {
+		if index >= len(target) || target[index] != i {
+			ans = append(ans, "Push", "Pop")
+		} else {
+			ans = append(ans, "ush")
+			index++
+		}
+	}
+	return
+}
+
 func TestBuildArray(t *testing.T) {
 	fmt.Println(buildArray([]int{1, 3}, 3))
 	fmt.Println(buildArray([]int{1, 2, 3}, 3))
