10a4bfbb90b7c277b0ca56e8552e6465d419e924
lomtom <lomtom@qq.com>
2023-01-01 22:40:10 +0800

    剑指 Offer 03. 数组中重复的数字
    Signed-off-by: lomtom <lomtom@qq.com>

---
+++ leetcode/剑指 Offer 03. 数组中重复的数字_test.go
@@ -0,0 +1,13 @@
+package leetcode
+
+import "sort"
+
+func findRepeatNumber(nums []int) int {
+	sort.Ints(nums)
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i] == nums[i+1] {
+			return nums[i]
+		}
+	}
+	return 0
+}
