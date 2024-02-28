fa91f300cdd4ef5db593a8451eb04b1b1b7debe2
lomtom <lomtom@qq.com>
2024-02-28 10:14:00 +0800

    feat(代码): 236二叉树的最近公共祖先

---
+++ leetcode/236/solution.go
@@ -0,0 +1,21 @@
+package leetcode
+
+// 执行耗时:9 ms,击败了38.26% 的Go用户
+// 内存消耗:6.7 MB,击败了66.26% 的Go用户
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val == p.Val || root.Val == q.Val {
+		return root
+	}
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+	if left != nil && right != nil {
+		return root
+	}
+	if left == nil {
+		return right
+	}
+	return left
+}
