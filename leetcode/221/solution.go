096b65f49a35e1f8f2859a2fbde0bed3ab771848
lomtom <lomtom@qq.com>
2024-02-29 21:10:02 +0800

    feat(代码): 221最大正方形

---
+++ leetcode/221/solution.go
@@ -0,0 +1,36 @@
+package leetcode
+
+// 执行耗时:5 ms,击败了64.59% 的Go用户
+// 内存消耗:6.4 MB,击败了76.56% 的Go用户
+func maximalSquare(matrix [][]byte) int {
+	dp := make([][]int, len(matrix))
+	maxSide := 0
+	for i := 0; i < len(matrix); i++ {
+		dp[i] = make([]int, len(matrix[i]))
+		for j := 0; j < len(matrix[i]); j++ {
+			dp[i][j] = int(matrix[i][j] - '0')
+			if dp[i][j] == 1 {
+				maxSide = 1
+			}
+		}
+	}
+
+	for i := 1; i < len(matrix); i++ {
+		for j := 1; j < len(matrix[i]); j++ {
+			if dp[i][j] == 1 {
+				dp[i][j] = min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1
+				if dp[i][j] > maxSide {
+					maxSide = dp[i][j]
+				}
+			}
+		}
+	}
+	return maxSide * maxSide
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
---
+++ leetcode/221/solution_test.go
@@ -0,0 +1,12 @@
+package leetcode
+
+import "testing"
+
+func Test(t *testing.T) {
+	t.Log(maximalSquare([][]byte{
+		{'1', '0', '1', '0', '0'},
+		{'1', '0', '1', '1', '1'},
+		{'1', '1', '1', '1', '1'},
+		{'1', '0', '0', '1', '0'},
+	}))
+}
